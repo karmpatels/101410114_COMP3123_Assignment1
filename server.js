4d16bd8ddcacd199b949fbff4f4375a9e95cb13e
karmpatels <124847567+karmpatels@users.noreply.github.com>
2024-10-12 16:10:27 -0400

    some vercel setup done

--- server.js
+++ server.js
@@ -19,4 +19,10 @@ app.use('/api/v1/emp', employeeRoutes);
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
+// Home page route (this handles GET requests to "/")
+app.get('/', (req, res) => {
+  res.send('<h1>Welcome to My Website </h1><p>This is the home page for Vercel!</p>');
+});
+
+
 module.exports = app;
